d5aed45d793618081ea13755ce49928088649a1a
start.vaadin.com <no-reply@vaadin.com>
2022-11-03 13:43:44 +0000

    Generated project

---
+++ src/main/java/de/olafj/data/Role.java
@@ -0,0 +1,5 @@
+package de.olafj.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/de/olafj/data/service/UserRepository.java
@@ -0,0 +1,10 @@
+package de.olafj.data.service;
+
+import de.olafj.data.entity.User;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    User findByUsername(String username);
+}
